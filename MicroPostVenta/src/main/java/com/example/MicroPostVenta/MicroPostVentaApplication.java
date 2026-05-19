65420a2d9adf55bc78b774792021765e2df273a1
valerupayan12 <va.rupayan@duocuc.cl>
2026-05-19 13:32:41 -0400

    Add files via upload

---
+++ MicroPostVenta/src/main/java/com/example/MicroPostVenta/MicroPostVentaApplication.java
@@ -0,0 +1,13 @@
+package com.example.MicroPostVenta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroPostVentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroPostVentaApplication.class, args);
+	}
+
+}
---
+++ MicroPostVenta/src/test/java/com/example/MicroPostVenta/MicroPostVentaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MicroPostVenta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroPostVentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
