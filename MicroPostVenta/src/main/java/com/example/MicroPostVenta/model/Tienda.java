53c0cd809a7371b6dac5c1e3be079d47c66102ca
belenvelasco <bel.velasco@duocuc.cl>
2026-06-11 13:40:32 -0400

    002

--- MicroPostVenta/src/main/java/com/example/MicroPostVenta/model/Tienda.java
+++ MicroPostVenta/src/main/java/com/example/MicroPostVenta/model/Tienda.java
@@ -3,8 +3,6 @@ package com.example.MicroPostVenta.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -23,11 +21,9 @@ public class Tienda {
     private String nombre;
     @Column(name="direccion", nullable=false)
     private String direccion;
-    @ManyToOne
-    @JoinColumn(name="id_comuna", nullable=false)
+    @Column(name="comuna", nullable=false)
     private int comuna;
-    @ManyToOne
-    @JoinColumn(name="id_region", nullable=false)
+    @Column(name="region", nullable=false)
     private int region;
 
 }
